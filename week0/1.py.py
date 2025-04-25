b71d65aa8fc2f5d431712fa93e4feee803888eba
NencyAnghan3141 <nencyanghan10@gmail.com>
2025-04-25 16:51:13 -0700

    Add files via upload

---
+++ week0/1.py.py
@@ -0,0 +1,17 @@
+def square(x):
+    return x * x
+
+length = float(input("Enter the length of the land (can be negative or decimal): "))
+width = float(input("Enter the width of the land (can be negative or decimal): "))
+
+length = abs(length)
+width = abs(width)
+
+area = length * width
+
+rounded_area = round(area, 2)
+
+area_squared = square(area)
+
+print("Actual area of the rectangle:", rounded_area, "square units")
+print("Square of the area (for fun):", round(area_squared, 2))
