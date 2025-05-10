0d353d449434f6c861f03939e3e735edfad15147
NencyAnghan3141 <nencyanghan10@gmail.com>
2025-05-10 12:56:04 +1200

    Add files via upload

--- Desktop/MSE800/week5/database.py
+++ Desktop/MSE800/week5/database.py
@@ -2,6 +2,8 @@ import sqlite3
 
 def create_connection():
     conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+
     return conn
 
 def create_table():
@@ -14,5 +16,16 @@ def create_table():
             email TEXT NOT NULL UNIQUE
         )
     ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS courses (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT NOT NULL,
+            unit INTEGER NOT NULL
+        )
+    ''')
+    
     conn.commit()
     conn.close()
+
+    
