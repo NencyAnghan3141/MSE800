2dcce9a85c697d0d8bb045d10780715367b77e33
NencyAnghan3141 <nencyanghan10@gmail.com>
2025-05-10 12:57:17 +1200

    Add files via upload

--- Desktop/MSE800/week5/main.py
+++ Desktop/MSE800/week5/main.py
@@ -1,5 +1,5 @@
 from database import create_table
-from user_manager import add_user, view_users, search_user, delete_user, update_user
+from user_manager import add_user, view_users, search_user, delete_user, update_user, add_course, search_course_by_id, search_course_by_user_name
 
 def menu():
     print("\n==== User Manager ====")
@@ -8,13 +8,16 @@ def menu():
     print("3. Search User by Name")
     print("4. Delete User by ID")
     print("5. Update User by ID") 
-    print("6. Exit")
+    print("6. Add Course")
+    print("7. Search Course by ID")
+    print("8. Search Course by User Name")
+    print("9. Exit")
 
 def main():
-    create_table()
+    create_table() 
     while True:
         menu()
-        choice = input("Select an option (1-6): ")
+        choice = input("Select an option (1-9): ")
         if choice == '1':
             name = input("Enter name: ")
             email = input("Enter email: ")
@@ -37,6 +40,16 @@ def main():
             email = input("Enter new email: ")
             update_user(user_id, name, email)
         elif choice == '6':
+            course_name = input("Enter course name: ")
+            unit = int(input("Enter course unit: "))
+            add_course(course_name, unit)
+        elif choice == '7':
+            course_id = int(input("Enter ID to search: "))
+            search_course_by_id(course_id)
+        elif choice == '8':
+            user_name = input("Enter user name to search courses: ")
+            search_course_by_user_name(user_name)
+        elif choice == '9':
             print("Goodbye!")
             break
         else:
