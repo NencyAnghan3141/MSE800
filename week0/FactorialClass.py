938f92abe2fcfc015fa07747079689d56dc07aac
NencyAnghan3141 <nencyanghan10@gmail.com>
2025-05-25 19:09:59 +1200

    Factorial and Prime Class.py

--- week0/FactorialClass.py
+++ week0/FactorialClass.py
@@ -2,6 +2,15 @@ class Factorial:
     def __init__(self, num):
         self.num = num
 
+    def is_prime(self):
+        if self.num <= 1:
+            return False 
+        for i in range(2, int(self.num ** 0.5) + 1):  
+            if self.num % i == 0:
+             return False  
+        return True 
+
+
     def cal_fact(self):
         if self.num < 0:
             return "Factorial is not defined for negative numbers."
@@ -12,10 +21,19 @@ class Factorial:
             for i in range(2, self.num + 1):
                 result *= i
             return result
+        
+        
 
 
-num = 5
-factorial_obj = Factorial(num)
+# num = 5
+#factorial_obj = Factorial(num)
+# is_prime_obj = Factorial(num)
 
 
-print(f"The factorial of {num} is {factorial_obj.cal_fact()}")
+num = int(float(input('input a number ')))
+fac = Factorial(num)
+
+print(f"The factorial of {num} is {fac.cal_fact()}")
+print(f"The prime number of{num} is {fac.is_prime()}")
+
+
