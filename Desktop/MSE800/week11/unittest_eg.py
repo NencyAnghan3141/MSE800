af210f308a2cae5fd75668cd1dcafbcd18732312
NencyAnghan3141 <nencyanghan10@gmail.com>
2025-06-22 09:32:36 +1200

    Add files via upload

--- Desktop/MSE800/week11/unittest_eg.py
+++ Desktop/MSE800/week11/unittest_eg.py
@@ -1,15 +1,28 @@
+#A test assertion that checks whether the output equals the expected value.
+
+
+#def test_add(self):
+#This method checks the add() function with 2 test cases:
+#add(2, 3) should return 5
+#add(-1, 1) should return 0
+
+
+#If the result is not equal, the test fails and shows an error.
+#A test method. All test methods must start with test_ so the test runner recognizes them.
+
 import unittest
 
-# Function to be tested
+
+#Defines a function add(a, b)
 def add(a, b):
     return a + b
 
-# Test class
+#Defines a test class with one method test_add()
 class TestMathOperations(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
 
-# Run tests
+#Runs tests using unittest.main()
 if __name__ == '__main__':
     unittest.main()
